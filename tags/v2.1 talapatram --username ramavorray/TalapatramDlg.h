eb00fc9f9908cfc64cf9d0803e65706562d67139
ramavorray <ramavorray@1e59233a-b841-0410-9934-83d5419519a6>
2008-03-14 09:36:52 +0000

    tagging version 2.1

---
+++ tags/v2.1 talapatram --username ramavorray/TalapatramDlg.h
@@ -0,0 +1,20 @@
+/*-----------------------------
+Class: CTalapatramDlg
+
+This is the main class which holds all data of the application. Font information, all variables,
+all control information. Because of link inconsitency this class has been defined in Keyboard.h
+
+Written by: Rama Aravind Vorray, Feb 2007.
+-----------------------------*/
+
+#pragma once
+#include "afxwin.h"
+#include "DispBox.h"
+#include "Keyboard.h"
+#include "afxcmn.h"
+#include <string>
+
+using namespace std;
+
+
+//CTalapatramDlg is defined in Keyboard.h
